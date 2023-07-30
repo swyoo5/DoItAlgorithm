c58fbc902f05838cf1ee0e7f8a8f67fa12763418
sang won Yoo (유상원) <rongleader@naver.com>
2023-07-30 15:51:52 +0900

    초기 커밋.

---
+++ DoItAlgorithm/1-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b, c;
+	
+	cout << "세 정수의 최댓값을 구합니다.\n";
+	cout << "a값 : ";
+	cin >> a;
+
+	cout << "b값 : ";
+	cin >> b;
+
+	cout << "c값 : ";
+	cin >> c;
+
+	int max = a;
+	if (b > max) max = b;
+	if (c > max) max = c;
+
+	cout << "최댓값은 " << max << "입니다.\n";
+	return 0;
+}
